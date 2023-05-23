f077e21082ca115af628816c1fd51dd394f246f9
Den <dimas.dermawan@ui.ac.id>
2023-05-23 23:17:43 +0700

    Tambahin variabel color white

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'login-pattern': "url('src/assets/Frame 7 1.svg')",
-        'login-pattern-mobile': "url('src/assets/Frame 8 2.svg')"
+        "login-pattern": "url('src/assets/Frame 7 1.svg')",
+        "login-pattern-mobile": "url('src/assets/Frame 8 2.svg')",
       },
       fontFamily: {
-        'Montserrat': ['Montserrat', 'sans-serrif'],
-        'Inter' : ['Inter', 'sans-serrif']
-    },
-     colors: {
-        'orange-sempoa': '#FB9E23',
-      }
+        Montserrat: ["Montserrat", "sans-serrif"],
+        Inter: ["Inter", "sans-serrif"],
+      },
+      colors: {
+        "orange-sempoa": "#FB9E23",
+        white: "#FAFAFA",
+      },
     },
   },
   plugins: [require("daisyui")],
