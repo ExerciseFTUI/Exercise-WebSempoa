d81c54db6e199a17b12eb12b52c3a96369554980
Den <dimas.dermawan@ui.ac.id>
2023-05-30 10:29:01 +0700

    Add some line and formatting

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,64 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { BsArrowLeftShort, BsPeople, BsPencilSquare, BsCardHeading } from "react-icons/bs";
-import { BiPackage } from "react-icons/bi";
-import { IoShirtOutline } from "react-icons/io5";
-import sempoaLogo from "../assets/sempoa-logo.png";
-import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios"
+import React, { useState } from "react"
+import { useEffect } from "react"
+import {
+  BsArrowLeftShort,
+  BsPeople,
+  BsPencilSquare,
+  BsCardHeading,
+} from "react-icons/bs"
+import { BiPackage } from "react-icons/bi"
+import { IoShirtOutline } from "react-icons/io5"
+import sempoaLogo from "../assets/sempoa-logo.png"
+import { useNavigate, useParams } from "react-router-dom"
 
 export default function Sidebar() {
-  const navigate = useNavigate();
-  let { page } = useParams();
-  const [open, setOpen] = useState(true);
+  const navigate = useNavigate()
+
+  let { page } = useParams()
+
+  const [open, setOpen] = useState(true)
+
   const handleMouseEnter = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleMouseLeave = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const Menus = [
-    { id: "DaftarMurid", title: "Daftar Murid", src: "BsPeople", link: "/DaftarMurid" },
-    { id: "Pendaftaran", title: "Pendaftaran", src: "BsPencilSquare", link: "/Pendaftaran" },
+    {
+      id: "DaftarMurid",
+      title: "Daftar Murid",
+      src: "BsPeople",
+      link: "/DaftarMurid",
+    },
+    {
+      id: "Pendaftaran",
+      title: "Pendaftaran",
+      src: "BsPencilSquare",
+      link: "/Pendaftaran",
+    },
     { id: "Barang", title: "Barang", src: "BiPackage", link: "/Barang" },
     { id: "Voucher", title: "Voucher", src: "BsCardHeading", link: "/Voucher" },
-    { id: "Merchandise", title: "Merchandise", src: "IoShirtOutline", link: "/Merchandise" },
-  ];
+    {
+      id: "Merchandise",
+      title: "Merchandise",
+      src: "IoShirtOutline",
+      link: "/Merchandise",
+    },
+  ]
+
+  /* prettier-ignore */
   return (
     <div className={`flex flex-col justify-center space-y-6 relative bg-[#FAFAFA] h-screen mt-0 p-5 pt-0 ${open ? "w-[60%] sm:w-[45%] md:w-[30%] lg:w-[20%]" : "w-[25%] sm:w-[20%] md:w-[15%] lg:w-[10%]"}  duration-300 sticky top-0`}>
       <BsArrowLeftShort className={`text-3xl absolute -right-3 top-9 text-white bg-[#FB9E23] rounded-full border border-white cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
+
       <div className="h-80 flex justify-center pt-12 items-center">
         <img src={sempoaLogo} className={`absolute w-[90%] duration-500 ${open ? "translate-x-0 ease-out" : "-translate-x-[200%] ease-in"}`} alt="Sempoa logo" />
       </div>
+
       <div className="pt-14">
         <ul>
           {Menus.map((menu, index) => {
@@ -41,10 +68,9 @@ export default function Sidebar() {
               !open && setOpen(!open);
             };
             return (
-              <div className="h-16">
+              <div key={index} className="h-16">
                 <li
-                  key={index}
-                  className={`peer flex flex-auto mx-auto items-center ${
+                  className={`peer flex flex-auto mx-auto items-center justify-between ${
                     open ? "w-[90%]" : "w-16"
                   } duration-500 h-14 rounded-2xl border-solid text-[#ED7336] border-[#ED7336] hover:text-[#FAFAFA] hover:bg-[#ED7336] border-2 cursor-pointer ${menu.id === page && "bg-[#ED7336] text-[#FAFAFA]"}`}
                   onClick={handleClick}>
@@ -57,6 +83,7 @@ export default function Sidebar() {
           })}
         </ul>
       </div>
+      
       <div className="h-screen pt-11">
         <div className={`flex mx-auto items-center ${open ? "w-[90%]" : "w-16"} duration-500 h-16 rounded-2xl border-solid border-black border-2 cursor-pointer`}>
           <div className={`avatar placeholder ${open ? "m-3" : "m-auto"}`}>
