af118ec410a5c3fc59cee4f0ae97840b82c45f4f
albertustimothyy <albertustimothyy@gmail.com>
2023-06-01 13:16:14 +0700

    bikin page barang buku

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createBrowserRouter, Navigate, RouterProvider, createRoutesFromElements } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -9,18 +9,19 @@ import ErrorPage from "./pages/ErrorPage";
 import LoginPage from "./pages/LoginPage";
 import PilihCabang from "./pages/PilihCabang";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<LoginPage />} />
+      <Route path="cabang" element={<PilihCabang />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="dashboard/:page" element={<Dashboard />} />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="cabang" element={<PilihCabang />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="dashboard/:page" element={<Dashboard />} />
-        </Route>
-        <Route path="/*" element={<Navigate to="/dashboard" />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/sub_pages/Barang.jsx
+++ src/pages/sub_pages/Barang.jsx
@@ -1,7 +1,104 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { CiSearch } from "react-icons/ci";
 
-export default class Barang extends Component {
-  render() {
-    return <div>Barang</div>;
-  }
-}
+const Barang = () => {
+  const [barang, setBarang] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("tersedia");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    let endpoint = "";
+
+    if (selectedOption === "tersedia") {
+      endpoint = "link1";
+    } else if (selectedOption === "terjual") {
+      endpoint = "link2";
+    } else if (selectedOption === "rusak") {
+      endpoint = "link3";
+    }
+
+    try {
+      const { data } = await axios.get(endpoint);
+      setBarang(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClick = (option) => {
+    setSelectedOption(option);
+  };
+  return (
+    <div className="flex-auto bg-[#fb9e23]">
+      <div className="bg-[#ed7336] font-Inter text-[#FAFAFA] p-6 px-12 space-y-2">
+        <h1 className="font-bold text-2xl">Cabang A</h1>
+        <h2 className="font-extralight text-md">Kota, Provinsi</h2>
+      </div>
+      <div className="px-12 p-6 space-y-6">
+        <div className="flex flex-row space-x-16">
+          <div className="flex flex-row items-center space-x-2">
+            <input type="search" className="rounded-3xl px-5 p-2 focus:outline-none" placeholder="Search" />
+            <CiSearch className="text-4xl text-[#FAFAFA]" />
+          </div>
+          <div className="flex flex-row space-x-10">
+            <div
+              className={`bg-[#FAFAFA] text-xl p-[6px] px-10 my-auto rounded-3xl hover:text-orange-sempoa cursor-pointer border-2 border-[#FAFAFA] ${selectedOption === "tersedia" ? "bg-orange-sempoa-dark text-[#FAFAFA]" : ""}`}
+              onClick={() => handleClick("tersedia")}>
+              Tersedia
+            </div>
+            <div
+              className={`bg-[#FAFAFA] text-xl p-[6px] px-10 my-auto rounded-3xl hover:text-orange-sempoa cursor-pointer border-2 border-[#FAFAFA] ${selectedOption === "terjual" ? "bg-orange-sempoa-dark text-[#FAFAFA]" : ""}`}
+              onClick={() => handleClick("terjual")}>
+              Terjual
+            </div>
+            <div
+              className={`bg-[#FAFAFA] text-xl p-[6px] px-10 my-auto rounded-3xl hover:text-orange-sempoa cursor-pointer border-2 border-[#FAFAFA] ${selectedOption === "rusak" ? "bg-orange-sempoa-dark text-[#FAFAFA]" : ""}`}
+              onClick={() => handleClick("rusak")}>
+              Rusak
+            </div>
+          </div>
+        </div>
+
+        <div className="overflow-x-auto bg-[#FAFAFA] rounded-3xl">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th className="normal-case text-lg">ID</th>
+                <th className="normal-case text-lg">Nama Buku</th>
+                <th className="normal-case text-lg">Level</th>
+                <th className="normal-case text-lg">Tanggal Keluar</th>
+                <th className="normal-case text-lg">Pembeli</th>
+                <th className="normal-case text-lg">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {barang.map((data) => {
+                return (
+                  <tr className="border-hidden hover text-black" key={data.id}>
+                    <th></th>
+                    <td>{data._id}</td>
+                    <td>{data.nama}</td>
+                    <td>{data.level_sekarang}</td>
+                    <td>{data.email}</td>
+                    <td>{data.email}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Barang;
