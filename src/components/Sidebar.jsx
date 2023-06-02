7586a5be9d0d77cc98a29f0488ad4b600a32bfae
Den <dimas.dermawan@ui.ac.id>
2023-06-02 18:29:40 +0700

    change styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
-import axios from "axios"
 import React, { useState } from "react"
-import { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+import axios from "axios"
+
 import {
   BsArrowLeftShort,
   BsPeople,
@@ -10,98 +11,89 @@ import {
 } from "react-icons/bs"
 import { BiPackage } from "react-icons/bi"
 import { IoShirtOutline } from "react-icons/io5"
+
 import sempoaLogo from "../assets/sempoa-logo.png"
-import { useNavigate, useParams } from "react-router-dom"
 
 export default function Sidebar() {
-  const navigate = useNavigate()
-
-  let { page } = useParams()
-
   const [open, setOpen] = useState(true)
 
-  const handleMouseEnter = () => {
-    setOpen(true)
-  }
+  let { pathname } = useLocation()
 
-  const handleMouseLeave = () => {
-    setOpen(false)
-  }
+  const page = pathname.split("/")[2]
 
   const Menus = [
-    {
-      id: "DaftarMurid",
-      title: "Daftar Murid",
-      src: "BsPeople",
-      link: "/DaftarMurid",
-    },
-    {
-      id: "Pendaftaran",
-      title: "Pendaftaran",
-      src: "BsPencilSquare",
-      link: "/Pendaftaran",
-    },
-    { id: "Barang", title: "Barang", src: "BiPackage", link: "/Barang" },
-    { id: "Voucher", title: "Voucher", src: "BsCardHeading", link: "/Voucher" },
-    {
-      id: "Merchandise",
-      title: "Merchandise",
-      src: "IoShirtOutline",
-      link: "/Merchandise",
-    },
+    { title: "Daftar Murid", src: "BsPeople" },
+    { title: "Pendaftaran", src: "BsPencilSquare" },
+    { title: "Barang", src: "BiPackage" },
+    { title: "Voucher", src: "BsCardHeading" },
+    { title: "Merchandise", src: "IoShirtOutline" },
   ]
+
+  const navigate = useNavigate()
 
   /* prettier-ignore */
   return (
-    <div className={`flex flex-col justify-center space-y-6 relative bg-[#FAFAFA] h-screen mt-0 p-5 pt-0 ${open ? "w-[60%] sm:w-[45%] md:w-[30%] lg:w-[20%]" : "w-[25%] sm:w-[20%] md:w-[15%] lg:w-[10%]"}  duration-300 sticky top-0`}>
-      <BsArrowLeftShort className={`text-3xl absolute -right-3 top-9 text-white bg-[#FB9E23] rounded-full border border-white cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
+    <div
+      id="sidebar"
+      className={`bg-white duration-300 flex flex-col items-center justify-between sticky top-0 h-screen py-5 ${open ? "w-[60%] sm:w-[45%] md:w-[30%] lg:w-[20%]" : "w-[25%] sm:w-[20%] md:w-[15%] lg:w-[10%]"} `}
+    >
+      <BsArrowLeftShort
+        className={`text-3xl absolute -right-3 top-9 text-white bg-orange-sempoa rounded-full border border-white cursor-pointer ${!open && "rotate-180"} z-10 duration-300`}
+        onClick={() => setOpen(!open)}
+      />
 
-      <div className="h-80 flex justify-center pt-12 items-center">
-        <img src={sempoaLogo} className={`absolute w-[90%] duration-500 ${open ? "translate-x-0 ease-out" : "-translate-x-[200%] ease-in"}`} alt="Sempoa logo" />
-      </div>
+      <img
+        src={sempoaLogo}
+        className={`relative w-80 duration-500 ${open ? "translate-x-0 ease-out" : "-translate-x-[200%] ease-in"}`}
+        alt="Sempoa logo"
+      />
 
-      <div className="pt-14">
-        <ul>
-          {Menus.map((menu, index) => {
-            const Icon = eval(menu.src);
-            const handleClick = () => {
-              navigate(`/Dashboard${menu.link}`, { state: { relative: true } });
-              !open && setOpen(!open);
-            };
-            return (
-              <div key={index} className="h-16">
-                <li
-                  key={index}
-                  className={`peer flex flex-auto mx-auto pr-5 items-center justify-between ${
-                    open ? "w-[90%]" : "w-16"
-                  } duration-500 h-14 rounded-2xl border-solid text-[#ED7336] border-[#ED7336] hover:text-[#FAFAFA] hover:bg-[#ED7336] border-2 cursor-pointer ${menu.id === page && "bg-[#ED7336] text-[#FAFAFA]"}`}
-                  onClick={handleClick}>
-                  <div className="flex items-center">
-                    <Icon className={`text-2xl ${open ? "m-2 ml-10" : "m-auto"} `} />
-                    <span className={`text-base ${!open && "hidden"}`}>{menu.title}</span>
-                  </div>
-                  {menu.id === "Barang" && <BsChevronDown className="text-2xl" />}
-                </li>
-                {menu.id === page && <div className="peer-visible h-8 w-8 bg-[#ED7336] rounded-lg relative -left-11 -top-11" />}
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-      
-      <div className="h-screen pt-11">
-        <div className={`flex mx-auto items-center ${open ? "w-[90%]" : "w-16"} duration-500 h-16 rounded-2xl border-solid border-black border-2 cursor-pointer`}>
-          <div className={`avatar placeholder ${open ? "m-3" : "m-auto"}`}>
-            <div className="bg-[#ED7336] text-neutral-content rounded-full w-11" />
+      <ul className="space-y-2 mb-16">
+        {Menus.map(({ title, src }, i) => {
+          const Icon = eval(src)
+          const id = title.toLowerCase().replace(" ", "-")
+
+          const link = id == "barang" ? `/dashboard/barang/pemesanan` : `/dashboard/${id}`
+
+          const handleClick = () =>
+            navigate(link, { state: { relative: true } })
+
+          return (
+            <li
+              key={i}
+              className={`border-orange-darker border-2 rounded-2xl cursor-pointer hover:text-white hover:bg-orange-darker duration-500 ${
+                id === page
+                  ? "bg-orange-darker text-white"
+                  : "text-orange-darker"
+              } ${
+                open ? "w-60" : "w-12"
+              } h-12 relative flex items-center gap-2`}
+              onClick={handleClick}
+            >
+              <Icon className={`text-2xl ${open ? "m-2 ml-10" : "m-auto"} `} />
+
+              <span className={`text-base ${!open && "hidden"}`}>{title}</span>
+
+              {/* {id === "barang" && open && <BsChevronDown className="text-xl ml-8" />} */}
+
+              {/* {id === page && (
+                <div className="h-8 w-8 bg-orange-darker rounded-lg absolute top-1 -left-11" />
+              )} */}
+            </li>
+          )
+        })}
+      </ul>
+
+      <div className={`border-black border-2 cursor-pointer duration-500 px-5 h-16 rounded-2xl flex items-center justify-center gap-5`}>
+        <div className="bg-orange-darker bg-opacity-90 text-neutral-content rounded-full w-11 h-11" />
+
+        {open && (
+          <div>
+            <p className="font-bold">Nama User</p>
+            <p className="text-xs">Status User</p>
           </div>
-          {open && (
-            <div>
-              <p className="font-bold">Nama User</p>
-              <p className="text-xs">Status User</p>
-            </div>
-          )}
-        </div>
+        )}
       </div>
     </div>
-  );
+  )
 }
