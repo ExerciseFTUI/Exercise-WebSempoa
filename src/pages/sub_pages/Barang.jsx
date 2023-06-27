e29d33d1e75602a2c7a677dd243c6f47f5c7c28f
RadityaDito <raditya.ihsan@ui.ac.id>
2023-06-27 14:16:12 +0700

    Solving Conflict

--- src/pages/sub_pages/Barang.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Barang extends Component {
-  render() {
-    return <div>Barang</div>;
-  }
-}
--- src/pages/sub_pages/Merchandise.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Merchandise extends Component {
-  render() {
-    return <div>Merchandise</div>;
-  }
-}
