4e28d94c6a5ba4311347f2ab27673cc5eb410a7d
Den <dimas.dermawan@ui.ac.id>
2023-07-04 23:21:43 +0700

    Add /daftar-civitas/murid and /daftar-civitas/guru routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-import App from "./App";
-import Dashboard from "./pages/Dashboard";
-import ErrorPage from "./pages/Error";
-import LoginPage from "./pages/Login";
-import PilihCabang from "./pages/PilihCabang";
-import Barang from "./pages/Dashboard/Barang";
+import App from "./App"
+import Dashboard from "./pages/Dashboard"
+import ErrorPage from "./pages/Error"
+import LoginPage from "./pages/Login"
+import PilihCabang from "./pages/PilihCabang"
 
-import Voucher from "./pages/Dashboard/Voucher";
-import Merchandise from "./pages/Dashboard/Merchandise";
-import DaftarMurid from "./pages/Dashboard/DaftarMurid";
-import Pendaftaran from "./pages/Dashboard/Pendaftaran";
-import DataBuku from "./pages/Dashboard/Barang/DataBuku";
-import DataPemesanan from "./pages/Dashboard/Barang/DataPemesanan";
+import Voucher from "./pages/Dashboard/Voucher"
+import Merchandise from "./pages/Dashboard/Merchandise"
+import Pendaftaran from "./pages/Dashboard/Pendaftaran"
 
-import "./index.css";
+import Civitas from "./pages/Dashboard/Civitas"
+import DaftarMurid from "./pages/Dashboard/Civitas/DaftarMurid"
+import DaftarGuru from "./pages/Dashboard/Civitas/DaftarGuru"
+
+import Barang from "./pages/Dashboard/Barang"
+import DataBuku from "./pages/Dashboard/Barang/DataBuku"
+import DataPemesanan from "./pages/Dashboard/Barang/DataPemesanan"
+
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -27,7 +31,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
         <Route path="/" element={<App />}>
           <Route path="dashboard/" element={<Dashboard />}>
-            <Route path="daftar-murid" element={<DaftarMurid />} />
+            <Route path="daftar-civitas/" element={<Civitas />}>
+              <Route path="murid" element={<DaftarMurid />} />
+              <Route path="guru" element={<DaftarGuru />} />
+            </Route>
+
             <Route path="pendaftaran" element={<Pendaftaran />} />
 
             <Route path="barang/" element={<Barang />}>
@@ -45,4 +53,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
