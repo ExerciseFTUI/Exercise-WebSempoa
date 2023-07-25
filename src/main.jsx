11f85d6623778c4baef9c5b2b7d6173293d09627
RadityaDito <raditya.ihsan@ui.ac.id>
2023-07-26 01:09:51 +0700

    Fix Import Bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,35 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { UserContextProvider } from "./components/Contexts/UserContext"
-
-import App from "./App"
-
-import Dashboard from "./pages/Dashboard"
-import ErrorPage from "./pages/Error"
-import LoginPage from "./pages/Login"
-
-import PilihCabang from "./pages/PilihCabang"
-import TambahCabang from "./pages/PilihCabang/TambahCabang"
-
-import Voucher from "./pages/Dashboard/Voucher"
-import Merchandise from "./pages/Dashboard/Merchandise"
-import Pendaftaran from "./pages/Dashboard/Pendaftaran"
-
-
-import Civitas from "./pages/Dashboard/Civitas"
-import DaftarMurid from "./pages/Dashboard/Civitas/DaftarMurid"
-import DaftarGuru from "./pages/Dashboard/Civitas/DaftarGuru"
-import TambahGuru from "./pages/Dashboard/Civitas/TambahGuru"
-
-import Barang from "./pages/Dashboard/Barang"
-import DataBuku from "./pages/Dashboard/Barang/DataBuku"
-import DataPemesanan from "./pages/Dashboard/Barang/DataPemesanan"
-import Pembayaran from "./pages/Dashboard/Pembayaran"
-import Invoice from "./pages/Dashboard/Pembayaran/Invoice"
-
-import TambahAdmin from "./pages/TambahAdmin"
-
-import "./index.css"
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { UserContextProvider } from "./components/Contexts/UserContext";
+
+import App from "./App";
+
+import Dashboard from "./pages/Dashboard";
+import ErrorPage from "./pages/Error";
+import LoginPage from "./pages/Login";
+
+import PilihCabang from "./pages/PilihCabang";
 import TambahCabang from "./pages/PilihCabang/TambahCabang";
 
+import Voucher from "./pages/Dashboard/Voucher";
+import Merchandise from "./pages/Dashboard/Merchandise";
+import Pendaftaran from "./pages/Dashboard/Pendaftaran";
+
+import Civitas from "./pages/Dashboard/Civitas";
+import DaftarMurid from "./pages/Dashboard/Civitas/DaftarMurid";
+import DaftarGuru from "./pages/Dashboard/Civitas/DaftarGuru";
+import TambahGuru from "./pages/Dashboard/Civitas/TambahGuru";
+
+import Barang from "./pages/Dashboard/Barang";
+import DataBuku from "./pages/Dashboard/Barang/DataBuku";
+import DataPemesanan from "./pages/Dashboard/Barang/DataPemesanan";
+import Pembayaran from "./pages/Dashboard/Pembayaran";
+import Invoice from "./pages/Dashboard/Pembayaran/Invoice";
+
+import TambahAdmin from "./pages/TambahAdmin";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -75,4 +70,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </UserContextProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
