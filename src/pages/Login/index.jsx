9cd6b3141722b9b014ed064bff7ae387e4832baa
Den <dimas.dermawan@ui.ac.id>
2023-07-25 20:57:06 +0700

    FIX login background vector not showing up

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from "react"
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai"
 
+import { UserContext } from "../../components/Contexts/UserContext"
+
 import sempoaLogo from "../../assets/sempoa-logo.png"
 import exerciseLogo from "../../assets/Exercise Logo.png"
-import { UserContext } from "../../components/Contexts/UserContext"
+import vector from "../../assets/Frame 7 1.svg"
+import vectorMobile from "../../assets/Frame 8 2.svg"
 
 const LoginPage = (props) => {
   //Global State
@@ -22,23 +25,38 @@ const LoginPage = (props) => {
   }
 
   return (
-    <div className="sm:bg-login-pattern bg-login-pattern-mobile bg-contain bg-no-repeat w-full h-screen bg-[#FAFAFA]">
+    <div className="bg-contain bg-no-repeat w-full h-screen py-12 md:py-0 overflow-hidden bg-white">
       <img
         src={exerciseLogo}
         className="absolute bottom-4 right-0"
         alt="Exercise"
       />
-      <div className="flex flex-col md:flex-row items-center md:justify-around mx-auto h-screen">
+
+      <img
+        src={vector}
+        alt="Background Vector"
+        className="absolute top-0 left-0 h-screen object-cover z-[1] hidden md:block"
+      />
+
+      <img
+        src={vectorMobile}
+        alt="Background Vector"
+        className="absolute top-0 left-0 h-screen object-cover z-[1] md:hidden"
+      />
+
+      <div className="flex flex-col xl:flex-row items-center xl:justify-around w-full h-screen gap-12 md:pt-12 xl:pt-0">
         <img
           src={sempoaLogo}
-          className="lg:h-[320px] md:h-[200px] h-[136px]"
+          className="lg:h-[240px] md:h-[160px] h-[136px] z-[2]"
           alt="logo"
         />
-        <div className="sm:mr-24 backdrop-blur-sm rounded-2xl min-w-[20rem] min-h-[24rem] sm:w-[26rem] sm:h-[30rem] border-2 border-orange-sempoa shadow-2xl border-solid border-opacity-100">
+
+        <div className="backdrop-blur-sm rounded-2xl min-w-[20rem] min-h-[24rem] sm:w-[26rem] sm:h-[30rem] border-2 border-orange-sempoa shadow-2xl border-solid border-opacity-100 bg-white bg-opacity-95 z-[2]">
           <div className="my-10 space-y-4 sm:p-8">
             <h1 className="text-4xl sm:text-5xl font-bold leading-tight tracking-tight text-black text-center font-Montserrat">
               Log In
             </h1>
+
             <form className="space-y-4" action="#" onSubmit={handleSubmit}>
               <div className="flex justify-center">
                 <input
@@ -106,6 +124,7 @@ const LoginPage = (props) => {
                 </button>
               </div>
             </form>
+
             <p className="sm:p-1.5 p-1 text-sm text-center font-Montserrat text-gray-800">
               Do not have account?{" "}
               <a
